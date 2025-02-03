843b00070af4cdfef6f7518783dc5db4e3123258
murtuzajambughoda <murtuza.jambughoda@moschip.com>
2025-02-03 17:45:20 +0530

    some changes

--- myserial.py
+++ myserial.py
@@ -54,5 +54,4 @@ class MySerial:
         file_handler = logging.FileHandler(self.log_file)
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
-        logger1 = 0
         return logger
