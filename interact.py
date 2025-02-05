50791d988ca135cf40131f13d4646a62ec87209f
murtuzajambughoda <murtuza.jambughoda@moschip.com>
2025-02-05 14:17:20 +0530

    changes4

--- interact.py
+++ interact.py
@@ -1,9 +1,22 @@
+
+"""Module for serial communication using myserial."""
+
+# pylint: disable=invalid-name
+
 import myserial
 
 serial_connect = myserial.MySerial("COM1")
-serial_connect.connect()
-serial_connect.write(b"Hello")
-data = serial_connect.read()
-print("Read data:", data)
-serial_connect.flush()
-serial_connect.close()
+
+
+def main():
+    """Establish a connection, send data, read response, and clean up."""
+    serial_connect.connect()
+    serial_connect.write(b"Hello")
+    data = serial_connect.read()
+    print("Read data:", data)
+    serial_connect.flush()
+    serial_connect.close()
+
+
+if __name__ == "__main__":
+    main()
