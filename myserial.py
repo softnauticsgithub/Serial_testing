83fb36809f170f03f7886fafaef91f36792ae330
MurtazaJ2 <murtuza.jambughoda@moschip.com>
2025-02-05 10:29:29 +0000

    Auto-corrected Python files

--- myserial.py
+++ myserial.py
@@ -106,7 +106,8 @@ class MySerial:
         logger.setLevel(logging.INFO)
         formatter = logging.Formatter(
             '%(asctime)s - %(levelname)s - %(message)s'
-        file_handler = logging.FileHandler(self.log_file
-        file_handler.setFormatter(formatter
-        logger.addHandler(file_handler
+        )
+        file_handler = logging.FileHandler(self.log_file)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
         return logger
